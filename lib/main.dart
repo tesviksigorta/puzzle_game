e9bc5f41ebe7bdabdb0c2ce2cf443da358289583
Deneme <tesviksigorta@gmail.com>
2022-06-04 23:23:15 +0300

    boşluk

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
-      ),
+      ), 
       home: const HomePage(),
     );
   }
